3554e6404c4f7f7c49925141eaca215cbcd542f9
Rachel Lai <therachellai@gmail.com>
2023-04-19 18:02:11 -0700

    something

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -6,7 +6,7 @@ public class Collatz {
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
         if (n  == 128) {
-            return 1;
+            return 12;
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
